4b969e07d003b738a0ecd1a9ee780584c7e32766
XINYIHE435 <xyhe187@163.com>
2025-08-10 00:37:10 +0800

    feat:0810联系人列表迁移

---
+++ 前端代码/codes/src/constants/contacts.js
@@ -0,0 +1,6 @@
+// src/constants/contacts.js
+export const ALL_CONTACTS = [
+  { id: 'CT1', name: '销售小孙', position: '销售代表', phone: '13811112222', email: 'xiaosun@example.com' },
+  { id: 'CT2', name: '销售小何', position: '销售代表', phone: '13822223333', email: 'xiaohe@example.com' },
+  { id: 'CT3', name: '销售组长凝凝子', position: '销售经理', phone: '13911112222', email: 'ningningzi@example.com' }
+];
